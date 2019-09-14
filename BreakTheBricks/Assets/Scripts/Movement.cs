22b42517b14e1d4bfebeffef799eaed75683f17a
tom1232289 <tom1232289@gmail.com>
2019-09-15 00:12:53 +0800

    BreakTheBricks_V0.1

---
+++ BreakTheBricks/Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float m_fSpeed = 3;
+
+    // Start is called before the first frame update
+    private void Start() {
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        float fHorizontal = Input.GetAxis("Horizontal");
+        float fVertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(fHorizontal, fVertical, 0) * Time.deltaTime * m_fSpeed);
+    }
+}
---
+++ BreakTheBricks/Assets/Scripts/Shoot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+    public GameObject m_bullet;
+    public float m_fSpeed = 5;
+
+    // Start is called before the first frame update
+    private void Start() {
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {  // 0表示鼠标左键
+            GameObject bullet = GameObject.Instantiate(m_bullet, transform.position, transform.rotation);
+            Rigidbody rigidBody = bullet.GetComponent<Rigidbody>();
+            rigidBody.velocity = transform.forward * m_fSpeed;
+        }
+    }
+}
